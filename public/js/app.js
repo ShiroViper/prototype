fbacab1464b617b0fe31233475aa38b96ed85bf2
Daniel <danjamo13@gmail.com>
2019-04-29 03:30:45 +0800

    Updated upload image for transaction

--- public/js/app.js
+++ public/js/app.js
@@ -36683,8 +36683,8 @@ if (token) {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! E:\prototype\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! E:\prototype\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\prototype\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\prototype\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
